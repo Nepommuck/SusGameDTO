30260fe06f6ba9295aee4c5d6434862dedc0b86a
Nepommuck <pawel.konopka5@gmail.com>
2024-12-02 22:32:03 +0100

    Minor DTO fix

--- src/main/kotlin/edu/agh/susgame/dto/rest/games/model/Response.kt
+++ src/main/kotlin/edu/agh/susgame/dto/rest/games/model/Response.kt
@@ -1,6 +1,8 @@
 package edu.agh.susgame.dto.rest.games.model
 
+import edu.agh.susgame.dto.rest.model.LobbyId
+
 
 data class GameCreationApiResponse(
-    val gameId: Int,
+    val lobbyId: LobbyId,
 )
